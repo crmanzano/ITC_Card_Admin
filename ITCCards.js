bfa6d0e844b1ebd0e326cfdf8e01e9718cddca62
crman <carlos.r.manzano@gmail.com>
2014-09-03 16:50:38 -0700

    ITCCardAdminProj
    ITCCardAdminProj

---
+++ ITCCards.js
@@ -0,0 +1,9 @@
+console.log("we will commence the batch card generator");
+
+alert("what is the first card number?");
+
+var CardNumber = prompt("enter the first CardNumber");
+alert("Thank you; what is the quantity of cards needed?");
+var quantity = prompt("enter the quantity");
+
+alert("your first CardNumber and quantity is " + CardNumber + quantity);
